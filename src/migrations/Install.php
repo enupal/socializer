387f3141b920fcb948562fc367cc75fae8ad07cc
Andre L <andrelopezd@gmail.com>
2019-12-09 16:11:36 -0400

    Adds install migration

---
+++ src/migrations/Install.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Socializer plugin for Craft CMS 3.x
+ *
+ * @link      https://enupal.com/
+ * @copyright Copyright (c) 2019 Enupal LLC
+ */
+
+namespace enupal\socializer\migrations;
+
+use craft\db\Migration;
+
+/**
+ * Installation Migration
+ */
+class Install extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->createTables();
+        $this->addForeignKeys();
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTableIfExists('{{%enupalsocializer_providers}}');
+
+        return true;
+    }
+
+    /**
+     * Creates the tables.
+     *
+     * @return void
+     */
+    protected function createTables()
+    {
+        $this->createTable('{{%enupalsocializer_providers}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'provider' => $this->string()->notNull(),
+            'clientId' => $this->string()->notNull(),
+            'clientSecret' => $this->string()->notNull(),
+            'fieldMapping' => $this->string(),
+            //
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
+    }
+
+    /**
+     * Adds the foreign keys.
+     *
+     * @return void
+     */
+    protected function addForeignKeys()
+    {
+        $this->addForeignKey(
+            $this->db->getForeignKeyName(
+                '{{%enupalsocializer_providers}}', 'id'
+            ),
+            '{{%enupalsocializer_providers}}', 'id',
+            '{{%elements}}', 'id', 'CASCADE', null
+        );
+    }
+}
