99e30ec8f28631f7e7384b53178dd61121cad8c7
Andre <andrelopezd@gmail.com>
2024-03-27 16:54:45 +0000

    Adds craft5 support

--- src/elements/Provider.php
+++ src/elements/Provider.php
@@ -282,7 +282,7 @@ class Provider extends Element
      * @throws \yii\base\InvalidConfigException
      * @throws \yii\base\InvalidConfigException
      */
-    protected function tableAttributeHtml(string $attribute): string
+    protected function attributeHtml(string $attribute): string
     {
         switch ($attribute) {
             case 'dateCreated':
